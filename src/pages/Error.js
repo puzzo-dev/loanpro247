16eb79e026e17261329a92843b4952204941d002
OG Gambino <og.puzzo@gmail.com>
2023-07-11 09:13:08 +0530

    error pages added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Frontpage from "./Frontpage";
 
 function Error() {
   return (
     <div>
-      
+      <div className="row justify-content-center">
+        <h1>404</h1>
+        <p>The Page you're looking for is not Found</p>
+      </div>
+      <Frontpage />
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
